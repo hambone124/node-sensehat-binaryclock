b8cb7e5b715f7638034db84a9f6c61be34d489c9
Hamilton Perez <hpe60m5@gmail.com>
2019-01-15 22:33:36 -0500

    Refactor display functions

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ const sense = require("sense-hat-led");
 const low = [50, 0, 50];
 const high = [0, 155, 155];
 const blank = [0, 0, 0];
-sense.setRotation(270, false);
+sense.setRotation(90, false);
 
 function convertToBinaryArray (num) {
     let binaryString = parseInt(num).toString(2).padStart(4,"0");
     let binaryArray = [];
     for (let char of binaryString) {
-        binaryArray.push(char);
+        // unshift here so the bits change in the right
+        // direction on the display
+        binaryArray.unshift(char);
     }
     return binaryArray;
 }
@@ -28,26 +30,61 @@ function getTimeStringArray () {
     }, []);
 }
 
-function tick() {
-    sense.flipV();
-    sense.flipH();
-    let timeArray = getTimeStringArray();
-    // let binaryTimeArray = timeArray.map(digit => convertToBinaryArray(digit));
-    let binaryTimeArray = timeArray.reduce((p, c) => {
-        let binaryDigitArray = convertToBinaryArray(c).concat(["x", "x", "x", "x"])
-        return p.concat(binaryDigitArray);
+function formatClockDisplay (_timeArray) {
+    // init the output array
+    let outputMatrix = [];
+
+    // convert numbers to binary form
+    let binaryTimeArray = _timeArray.reduce((p, c) => {
+        return p.concat(convertToBinaryArray(c));
     }, []);
-    let ledMatrix = (binaryTimeArray.concat(["x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x", "x"]).map(input => {
-        if(input === "0") {
-            return low;
-        }
-        else if (input === "1") {
-            return high;
-        } else {
-            return blank;
-        }            
-    }));
+
+    // spread out elements
+    binaryTimeArray.forEach( el => {
+        outputMatrix.unshift("x");
+        outputMatrix.unshift(el);
+    });
+
+    // add blank bars between hours, minutes, and seconds
+    for (let i = 0; i < 8; i++) {
+        outputMatrix.splice(16, 0, "x");
+    }
+    for (let i = 0; i < 8; i++) {
+        outputMatrix.splice(40, 0, "x");
+    }
+
+    return outputMatrix;
+}
+
+function outputToDisplay (matrix) {
+    let ledMatrix = matrix
+        .map(input => {
+            if(input === "0")
+                return low;
+            else if (input === "1")
+                return high;
+            else 
+                return blank;            
+        });
     sense.setPixels(ledMatrix);
+}
+
+function tick () {
+    let timeArray = getTimeStringArray();
+
+    // let ledMatrix = formatClockDisplay(timeArray)
+    //     .map(input => {
+    //         if(input === "0")
+    //             return low;
+    //         else if (input === "1")
+    //             return high;
+    //         else 
+    //             return blank;            
+    // });
+    // sense.setPixels(ledMatrix);
+    let formattedMatrix = formatClockDisplay(timeArray);
+    outputToDisplay(formattedMatrix);
+
 }
 
 setInterval(() => {
